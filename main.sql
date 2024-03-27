f3ac996e464dad7e8e1afa95082322803ccf23f9
MOYU <76060163+Moyu117@users.noreply.github.com>
2024-03-27 22:26:54 +0800

    Add files via upload

--- main.sql
+++ main.sql
@@ -1,44 +1,46 @@
 -- Pour activer les FKs  
-PRAGMA FOREIGN_KEYS=ON;
-CREATE TABLE Client(
-numCli INTEGER NOT NULL,
-nomCli TEXT NOT NULL,
-phone TEXT NOT NULL,
-CONSTRAINT PK_Client PRIMARY KEY (numCli)
+PRAGMA FOREIGN_KEYS=ON;  
+
+CREATE TABLE Client (
+    numCli INTEGER NOT NULL,
+    nomCli TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    CONSTRAINT PK_Client PRIMARY KEY (numCli)
 );
 
-CREATE TABLE Camera(
-numCam INTEGER NOT NULL,
-numM INTEGER NOT NULL,
-modele TEXT NOT NULL,
-dateRe DATE,
-qualite TEXT NOT NULL,
-prix REAL NOT NULL,
-CONSTRAINT PK_Camera PRIMARY KEY (numCam),
-CONSTRAINT CK_Qualite CHECK (qualite IN ('parfait','correct','tresbon'))
+CREATE TABLE Camera (
+    numCam INTEGER NOT NULL,
+    numM INTEGER NOT NULL,
+    modele TEXT NOT NULL,
+    dateRe DATE,
+    qualite TEXT NOT NULL,
+    prix REAL NOT NULL,
+    CONSTRAINT PK_Camera PRIMARY KEY (numCam),
+    CONSTRAINT FK_Camera_Marque FOREIGN KEY (numM) REFERENCES Marque(numM),
+    CONSTRAINT CK_Qualite CHECK (qualite IN ('parfait','correct','tresbon'))
 );
 
-CREATE TABLE Marque(
-numM INTEGER NOT NULL,
-nomM TEXT NOT NULL,
-descrp TEXT NOT NULL,
-CONSTRAINT PK_Marque PRIMARY KEY (numM)
+CREATE TABLE Marque (
+    numM INTEGER NOT NULL,
+    nomM TEXT NOT NULL,
+    descrp TEXT NOT NULL,
+    CONSTRAINT PK_Marque PRIMARY KEY (numM)
 );
 
-CREATE TABLE Dossier(
-numD INTEGER NOT NULL,
-numCli INTEGER NOT NULL,
-numCam INTEGER NOT NULL,
-dateVente DATE,
-CONSTRAINT PK_Dossier PRIMARY KEY (numD),
-CONSTRAINT FK_Dossier_Client FOREIGN KEY (numCli) REFERENCES Client(numCli),
-CONSTRAINT FK_Dossier_Camera FOREIGN KEY (numCam) REFERENCES Camera(numCam)
+CREATE TABLE Dossier (
+    numD INTEGER NOT NULL,
+    numCli INTEGER NOT NULL,
+    numCam INTEGER NOT NULL,
+    dateVente DATE,
+    CONSTRAINT PK_Dossier PRIMARY KEY (numD),
+    CONSTRAINT FK_Dossier_Client FOREIGN KEY (numCli) REFERENCES Client(numCli),
+    CONSTRAINT FK_Dossier_Camera FOREIGN KEY (numCam) REFERENCES Camera(numCam)
 );
 
-CREATE TABLE Wishlist(
-numCli INTEGER NOT NULL,
-numCam INTEGER NOT NULL,
-CONSTRAINT PK_Wishlist PRIMARY KEY (numCli, numCam),
-CONSTRAINT FK_Wishlist FOREIGN KEY (numCli) REFERENCES Client(numCli),
-CONSTRAINT FK_Wishlist FOREIGN KEY (numCam) REFERENCES Camera(numCam)
+CREATE TABLE  Wishlist (
+    numCli INTEGER NOT NULL,
+    numCam INTEGER NOT NULL,
+    CONSTRAINT PK_Wishlist PRIMARY KEY (numCli, numCam),
+    CONSTRAINT FK_Wishlist FOREIGN KEY (numCli) REFERENCES Client(numCli),
+    CONSTRAINT FK_Wishlist FOREIGN KEY (numCam) REFERENCES Camera(numCam)
 );
