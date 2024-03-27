9661b445966bb7f5fc3c9105534437107c885c2b
MOYU <76060163+Moyu117@users.noreply.github.com>
2024-03-27 21:39:16 +0800

    Update main.sql

--- main.sql
+++ main.sql
@@ -2,7 +2,37 @@
 PRAGMA FOREIGN_KEYS=ON;  
 
 --sql
-CREAT TABLE Client(
-id_Client INTEGER,
-nom_client CHAR,
-phone INTERGER,
+CREATE TABLE Client (
+    numCli INTEGER NOT NULL,
+    nomCli TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    CONSTRAINT PK_Client PRIMARY KEY (numCli)
+);
+
+CREATE TABLE Camera (
+    numCam INTEGER NOT NULL,
+    numM INTEGER NOT NULL,
+    modele TEXT NOT NULL,
+    dateRe DATE,
+    qualite TEXT NOT NULL,
+    prix REAL NOT NULL,
+    CONSTRAINT PK_Camera PRIMARY KEY (numCam),
+    CONSTRAINT CK_Qualite CHECK (qualite IN ('parfait','correct','tresbon'))
+);
+
+CREATE TABLE Marque (
+    numM INTEGER NOT NULL,
+    nomM TEXT NOT NULL,
+    descrp TEXT NOT NULL,
+    CONSTRAINT PK_Marque PRIMARY KEY (numM)
+);
+
+CREATE TABLE Dossier (
+    numD INTEGER NOT NULL,
+    numCli INTEGER NOT NULL,
+    numCam INTEGER NOT NULL,
+    dateVente DATE,
+    CONSTRAINT PK_Dossier PRIMARY KEY (numD),
+    CONSTRAINT FK_Dossier_Client FOREIGN KEY (numCli) REFERENCES Client(numCli),
+    CONSTRAINT FK_Dossier_Camera FOREIGN KEY (numCam) REFERENCES Camera(numCam)
+);
