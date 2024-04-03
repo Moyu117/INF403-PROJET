36e3bc3b3f2abec63c5cffd18df03a67f6890b1c
Ketchup <78511416+Ketchup-zzz@users.noreply.github.com>
2024-04-03 14:55:11 +0200

    202230403

--- Jeu_de_Test_OK.sql
+++ Jeu_de_Test_OK.sql
@@ -1,17 +1,24 @@
 -- Jeux de données OK (ça doit marcher)
 INSERT INTO Client VALUES ('10000001', 'Amy', '07 00 32 46 75');
 INSERT INTO Client VALUES ('10000002', 'Bob', '07 87 62 34 90');
+INSERT INTO Client VALUES ('10000003', 'Cathy', '07 00 32 46 75');
+INSERT INTO Client VALUES ('10000004', 'David', '07 87 62 34 90');
+INSERT INTO Client VALUES ('10000005', 'Eva', '07 00 32 46 75');
 
 
-INSERT INTO Camera VALUES ('1024', 'Canon', 'EOS R6 Mark II', '2020-12-03', 'parfait', '2000');
-INSERT INTO Camera VALUES ('1025', 'Nikon', 'D850', '2019-08-24', 'correct', '1500');
-INSERT INTO Camera VALUES ('1026', 'Sony', 'Alpha 7 III', '2021-02-08', 'tresbon', '1800');
-INSERT INTO Camera VALUES ('1027', 'Canon', 'EOS 5D Mark IV', '2018-09-08', 'correct', '2500');
+INSERT INTO Camera VALUES ('1024', '01', 'EOS R6 Mark II', '2020-12-03', 'parfait', '2000');
+INSERT INTO Camera VALUES ('1025', '02', 'D850', '2019-08-24', 'correct', '1500');
+INSERT INTO Camera VALUES ('1026', '03', 'Alpha 7 III', '2021-02-08', 'tresbon', '1800');
+INSERT INTO Camera VALUES ('1027', '01', 'EOS 5D Mark IV', '2018-09-08', 'correct', '2500');
+INSERT INTO Camera VALUES ('1028', '04', 'Lumix GH5', '2017-03-15', 'parfait', '1700');
 
 
 
-INSERT INTO Marque VALUES ('01', 'Canon', 'Canon est une marque japonaise spécialisée dans la fabrication d''appareils photo et de matériel photographique.');
-INSERT INTO Marque VALUES ('02', 'Nikon', 'Nikon est une marque japonaise spécialisée dans la fabrication d''appareils photo et de matériel photographique.');
+INSERT INTO Marque VALUES ('01', 'Canon', 'le premier choix d' 'innombrables amateurs et professionnels de la photographie.');
+INSERT INTO Marque VALUES ('02', 'Nikon', 'Un compagnon fidèle pour les photographes de paysages et de faune.');
+INSERT INTO Marque VALUES ('03', 'Sony', 'Un choix populaire pour les photographes de rue et de portrait.');
+INSERT INTO Marque VALUES ('04', 'Panasonic', 'Un choix populaire pour les vidéastes et les photographes de voyage.');
+INSERT INTO Marque VALUES ('05', 'Leica','il symbolise le luxe et la pureté des arts photographiques.');
 
 
 
