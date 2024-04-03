c8a84107d150700f994148263e49ba624bd6d3fe
Ketchup <78511416+Ketchup-zzz@users.noreply.github.com>
2024-04-03 15:33:31 +0200

    1

--- Jeu_de_Test_NOK.sql
+++ Jeu_de_Test_NOK.sql
@@ -1,3 +1,4 @@
 -- Jeux de données NOK (ne doit pas marcher àprès avoir éxécuté le jeux de données NOK)
 
 -- Erreur : Marque existante
+INSERT INTO Marque VALUES ('10', 'Canon', 'le premier choix d' 'innombrables amateurs et professionnels de la photographie.');
